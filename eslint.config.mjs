eb370e2dacdedccd360c3a63b045efb0a2872d7b
Lobe <lobe350@gmail.com>
2026-03-27 23:29:25 +0000

    feat(setup): add configuration files for linting, formatting, and commit message validation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,47 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import checkFile from "eslint-plugin-check-file";
+import nodePlugin from "eslint-plugin-n";
+import prettierConfig from "eslint-plugin-prettier/recommended";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  prettierConfig,
+  {
+    // optional: add this processor to files which not processed by other processors but still require linting
+    files: ["**/*.yaml", "**/*.webp"],
+    processor: "check-file/eslint-processor-check-file",
+  },
+  {
+    plugins: {
+      "check-file": checkFile,
+      n: nodePlugin,
+    },
+    rules: {
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "n/no-process-env": ["error"],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**": "NEXT_JS_APP_ROUTER_CASE",
+        },
+      ],
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
